163d62b3fc65acb1056b7e3b9810638e462192ee
dewang <dewang.wei@gmail.com>
2020-12-24 12:01:21 +0800

    change db connection method

--- e2error/error.go
+++ e2error/error.go
@@ -9,3 +9,12 @@ var (
 	ErrEmptyParameter   = func(msg string) error { return fmt.Errorf("parameter %v can't not empty", msg) }
 	ErrEmptyValue       = func(msg string) error { return fmt.Errorf("value %v empty", msg) }
 )
+
+func CheckErrors(errs ...error) error {
+	for idx := range errs {
+		if errs[idx] != nil {
+			return errs[idx]
+		}
+	}
+	return nil
+}
