45d905aedee36040aa01dacd6cce25ab63da980d
tk <io5499@gmail.com>
2024-05-16 01:13:43 -0700

    update

---
+++ e2db/util.go
@@ -0,0 +1,23 @@
+package e2db
+
+import (
+	"database/sql"
+
+	"github.com/sirupsen/logrus"
+	"gorm.io/gorm"
+)
+
+func MustGetCount(db *gorm.DB, model interface{}, query interface{}, args ...interface{}) sql.NullInt64 {
+	var c int64
+	if err := db.Model(model).Where(query, args).Count(&c).Error; err != nil {
+		logrus.Errorf("MustGetCount error=%v", err)
+		return sql.NullInt64{
+			Int64: 0,
+			Valid: false,
+		}
+	}
+	return sql.NullInt64{
+		Int64: c,
+		Valid: true,
+	}
+}
